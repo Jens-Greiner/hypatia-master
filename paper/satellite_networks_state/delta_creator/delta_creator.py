55448b429f0da008d4768d4aeea0afa1fbc1a94b
Jens Greiner <uwkzx@student.kit.edu>
2024-05-13 21:59:26 +0200

    changed delta script

--- paper/satellite_networks_state/delta_creator/delta_creator.py
+++ paper/satellite_networks_state/delta_creator/delta_creator.py
@@ -3,13 +3,12 @@ def read_file(file_path):
         return file.readlines()
 
 def compare_files(file1_lines, file2_lines):
-    common_lines = set(file1_lines).intersection(file2_lines)
+    exit_nodes = set(file1_lines).difference(file2_lines)
     only_second_file_lines = set(file2_lines).difference(file1_lines)
-    return sorted(common_lines), sorted(only_second_file_lines)
+    return sorted(exit_nodes), sorted(only_second_file_lines)
 
-def write_to_file(lines, output_file):
-    with open(output_file, 'w') as file:
-        file.writelines(lines)
+
+    
 
 def main():
     file1_path = 'GStopo2000-01-01 00:00:00.000.txt'
@@ -18,11 +17,14 @@ def main():
 
     file1_lines = read_file(file1_path)
     file2_lines = read_file(file2_path)
+    exit_nodes, only_second_file_lines = compare_files(file1_lines, file2_lines)
+    with open(output_file_path, 'a') as file:
+        file.write("#New Nodes: \n")
+        file.write("".join(only_second_file_lines))  
+        file.write("#Exit Nodes: \n")
+        file.write("".join(exit_nodes))  
 
-    common_lines, only_second_file_lines = compare_files(file1_lines, file2_lines)
-
-    new_file_lines = common_lines + only_second_file_lines
-    write_to_file(new_file_lines, output_file_path)
 
 if __name__ == "__main__":
     main()
+
